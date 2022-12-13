0b45297a9a20b39402057a1a81e78c455a3a34d4
Daniel M. Gomez <danielgomez7682@gmail.com>
2022-12-13 12:22:52 -0600

    updated gitignore file so that the binary files are ignored.

---
+++ cmd/tweet-img/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"sync"
+	"time"
+	"tweet-img/pkg/client"
+	"tweet-img/pkg/img"
+)
+
+func RandomSeed() int {
+	rand.Seed(time.Now().UnixNano())
+	min := 0
+	max := 1
+
+	randomInt := rand.Intn(max-min+1) + min
+	return randomInt
+}
+
+/* func UI() {
+	a := app.New()
+	w := a.NewWindow("Hello")
+
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
+
+	w.ShowAndRun()
+} */
+
+// Create a save option for a function so that a person is able to easily decide
+// if they want to save an image as well or not.
+
+func main() {
+	// UI()
+	start := time.Now()
+	urls := []string{
+		"https://picsum.photos/2560/1440",
+		// "https://picsum.photos/2560/1440",
+		"https://thecatapi.com/api/images/get?format=src&type=image&mime_types=jpg&size=full",
+		//"https://cataas.com/cat?width=2560&height=1440",
+		// "https://picsum.photos/1920/1080",
+	}
+
+	client, err := client.NewClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var wg sync.WaitGroup
+
+	var images [2]img.Image
+
+	for i := 0; i <= 1; i++ {
+		wg.Add(1)
+		go func(i int) {
+			seed := RandomSeed()
+			currentURL := urls[seed]
+			img, err := client.NewImg(currentURL)
+			if err != nil {
+				log.Fatal(err)
+			}
+			images[i] = img
+			defer wg.Done()
+		}(i)
+	}
+	wg.Wait()
+
+	joined, err := img.Join(images[0], images[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Send(*joined)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Elapsed: %v\n", time.Since(start))
+}
